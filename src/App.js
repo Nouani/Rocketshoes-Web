a5be643ffc972d4dcb2b952450020343b353dafc
Nouani <nouanigabriel@hotmail.com>
2020-02-12 21:26:06 -0300

    Estilização do Header

--- src/App.js
+++ src/App.js
@@ -4,10 +4,13 @@ import { BrowserRouter } from 'react-router-dom';
 import GlobalStyle from './styles/global';
 import Routes from './routes';
 
+import Header from './components/Header';
+
 function App() {
     return (
         <BrowserRouter>
             <GlobalStyle />
+            <Header />
             <Routes />
         </BrowserRouter>
     );
