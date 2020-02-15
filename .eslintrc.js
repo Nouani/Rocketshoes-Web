cf1bf0e7595e43042b3f1f41a1812fe61fa79843
Nouani <nouanigabriel@hotmail.com>
2020-02-15 19:23:21 -0300

    Configurando o reactotron com redux

--- .eslintrc.js
+++ .eslintrc.js
@@ -34,5 +34,6 @@ module.exports = {
     'import/prefer-default-export': 'off',
     'react/state-in-constructor': 'off',
     'react/static-property-placement': 'off',
+    'no-console': ["error", { allow: ["tron"] }]
   },
 };
