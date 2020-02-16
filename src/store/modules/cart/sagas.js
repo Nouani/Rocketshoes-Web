ffaa31be7a586c2ba25410bb548e508065a7879f
Nouani <nouanigabriel@hotmail.com>
2020-02-16 16:06:19 -0300

    Controle de estoque

--- src/store/modules/cart/sagas.js
+++ src/store/modules/cart/sagas.js
@@ -10,6 +10,18 @@ function* addToCart({ id }) {
         state.cart.find(p => p.id === id)
     );
 
+    const stock = yield call(api.get, `/stock/${id}`);
+
+    const stockAmount = stock.data.amount;
+    const currentAmount = productExists ? productExists.amount : 0;
+
+    const amount = currentAmount + 1;
+
+    if (amount > stockAmount) {
+        console.tron.warn('ERRO');
+        return;
+    }
+
     if (productExists) {
         yield put(updateAmount(id, productExists.amount + 1));
     } else {
