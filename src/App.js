8f7969c46c06bc118281df7e47d2db090d76f208
Nouani <nouanigabriel@hotmail.com>
2020-02-16 16:58:44 -0300

    Navegando no saga

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { ToastContainer } from 'react-toastify';
 
@@ -8,18 +8,19 @@ import './config/ReactotronConfig';
 import GlobalStyle from './styles/global';
 import Routes from './routes';
 import store from './store';
+import history from './services/history';
 
 import Header from './components/Header';
 
 function App() {
     return (
         <Provider store={store}>
-            <BrowserRouter>
+            <Router history={history}>
                 <GlobalStyle />
                 <Header />
                 <Routes />
                 <ToastContainer autoClose={3000} />
-            </BrowserRouter>
+            </Router>
         </Provider>
     );
 }
